5430d982f7979244c1f7e06db4bb105746db64cc
Xorice <mc5555195@outlook.com>
2024-07-15 14:24:49 +0800

    accomplished test.lua

--- src/test.lua
+++ src/test.lua
@@ -2,20 +2,34 @@
 
 local v3Seq = require(script:WaitForChild 'Vector3Sequence');
 
-
+-- Size Sequence
 local seqs = v3Seq.Bezier {
-	Vector3.new(0,0,0);
+	Vector3.new(0,0,1);
 	Vector3.new(4,4,4);
 	Vector3.new(0,0,0);
 }
 
+-- Position Sequence
 local seq = v3Seq.Bezier {
 	Vector3.new(0,4,4);
 	Vector3.new(20,4,4);
 	Vector3.new(12,4,-4);
 	Vector3.new(32,4,-4);
 }
+seq:ApplyEnvlopeFromBezier {0, 5, 0}
 
+-- Or try this
+--seq:ApplyEnvlope {0, 1, 2, 3, 4, 4, 3, 2, 1, 0}
+
+-- Tangent Sequence
+local seqt = v3Seq.BezierTan {
+	Vector3.new(0,4,4);
+	Vector3.new(20,4,4);
+	Vector3.new(12,4,-4);
+	Vector3.new(32,4,-4);
+}
+
+-- Test
 local sequence = v3Seq.simple {
 	0,   0,   0,   0;
 	0.1, 0.1, 0.1, 0.4;
@@ -37,10 +51,17 @@ end
 local timer = 0
 while true do
 	timer += task.wait();
-	
+	local seed = math.ceil(timer/2);
+	seed = nil; -- try to delete this line and see what would happen
+
 	for i, part in parts do
 		local alpha = (timer/2 + (i-1)/len)%1
+		
+		local vl = seqt:Value(alpha)
+		local vy = Vector3.new(0,1,0)
+		local vx = vl:Cross(vy);
+		
 		part.Size = seqs:Value(alpha)
-		part.CFrame = CFrame.new(seq:Value(alpha))
+		part.CFrame = CFrame.fromMatrix(seq:Value(alpha, seed), vx,vy,-vl)
 	end
 end
